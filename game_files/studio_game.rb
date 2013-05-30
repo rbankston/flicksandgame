10b255d0aea2df6ad48bf32045fe321197b42a39
Ralph Bankston <ralph.l.bankston@gmail.com>
2013-05-30 01:30:54 -0400

    Moved Classes out of the same file

--- game_files/studio_game.rb
+++ game_files/studio_game.rb
@@ -1,56 +1,5 @@
-class Player
-  attr_accessor :name
-  attr_reader :health
-  
-  def initialize(name, health=100)
-    @name = name.capitalize
-    @health = health
-  end
-  
-  def score
-    @name.length + @health
-  end
-
-  def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{score}"
-  end
-  
-  def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
-  end
-  
-  def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
-  end
-end
-
-class Game
-  def initialize(title)
-    @title = title
-    @players = []
-  end
-  
-  def add_player(a_player)
-    @players.push(a_player)
-  end
-  
-  attr_reader :title
-  
-  def play
-    puts "There are #{@players.count} in #{title}: "
-    @players.each do |player| 
-      puts player
-    end 
-    @players.each do |player|
-      player.blam
-      player.w00t
-      player.w00t
-      puts player
-    end
-  end
-end
+require_relative 'player'
+require_relative 'game'
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
