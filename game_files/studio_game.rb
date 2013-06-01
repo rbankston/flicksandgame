368b6c7b75aa71f4e20dd63d8669c2245390ae95
Ralph Bankston <ralph.l.bankston@gmail.com>
2013-06-01 00:59:30 -0400

    Added a player

--- game_files/studio_game.rb
+++ game_files/studio_game.rb
@@ -4,6 +4,7 @@ require_relative 'game'
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
+player4 = Player.new("shump", 45)
     
 knuckleheads = Game.new("Knuckleheads")
 knuckleheads.add_player(player1)
